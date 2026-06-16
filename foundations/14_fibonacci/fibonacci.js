0a000d42dca1a3ef2dc2a291d0bbb2a52ac0b189
kat-red <kauth2000@gmail.com>
2026-06-16 13:42:13 +0200

    Fibonacci Complete!

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+	if (n > 0){
+		let  fib = 1;
+		let prev = 0;
+		for (let i = 2; i<=n; i++){
+			let temp = fib;
+			fib += prev;
+			prev = temp;
+		}
+		return fib;
+	}
+	else if (n == 0)
+		return 0;
+	else return "OOPS";
 };
 
 // Do not edit below this line
