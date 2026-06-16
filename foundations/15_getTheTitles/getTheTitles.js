a67c73ee4db486e3eb1286f48e939c0dda4e92d9
kat-red <kauth2000@gmail.com>
2026-06-16 13:46:57 +0200

    Get The Titles Complete!

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+	let titles = [];
+	books.map(i=> titles.push(i.title))
+	return titles;
 };
 
 // Do not edit below this line
