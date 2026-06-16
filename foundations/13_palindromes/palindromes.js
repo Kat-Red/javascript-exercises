9032b1ab919ccc6ab362e7ac446b5c0e881723e6
kat-red <kauth2000@gmail.com>
2026-06-16 13:23:08 +0200

    Palindromes Compelete!

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,13 @@
-const palindromes = function () {
+const palindromes = function (str) {
+	const onlyAlph= str.split('').filter((i)=>{
+		if ((i.charCodeAt(0) >= 65 && i.charCodeAt(0) <= 90) || (i.charCodeAt(0) >= 97 && i.charCodeAt(0) <= 122) || (i.charCodeAt(0) >= 48 && i.charCodeAt(0) <= 57)){
+			return true;
+		}
+	}).join('').toLowerCase();
 
+	const reversed = onlyAlph.split('').reverse().join('');
+	return reversed == onlyAlph;
 };
-
+// ZZZZ car, a man, a maracaz.
 // Do not edit below this line
 module.exports = palindromes;
