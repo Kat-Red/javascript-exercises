43b3614609061a3fc60090061c8b0bddc922b5a5
kat-red <kauth2000@gmail.com>
2026-06-16 15:02:43 +0200

    Find The Oldest Compelete!

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+	const oldest = people.sort((a,b)=> {
+		let livedA;
+		if ("yearOfDeath" in a)
+			livedA = a.yearOfDeath - a.yearOfBirth
+		else livedA = (new Date()).getFullYear() - a.yearOfBirth;
+		let livedB;
+		if ("yearOfDeath" in b)
+			livedB = b.yearOfDeath - b.yearOfBirth
+		else livedB = (new Date()).getFullYear() - b.yearOfBirth;
+		return livedB-livedA;
+	});
+	return oldest[0];
 };
 
 // Do not edit below this line
